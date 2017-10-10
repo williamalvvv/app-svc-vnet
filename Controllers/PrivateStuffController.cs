910a6e9c923962350b896617a17ee9acc77c46b8
radu-matei <matei.radu94@gmail.com>
2017-10-10 20:57:41 +0300

    Add initial app
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ Controllers/PrivateStuffController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetcore_web.Controllers
+{
+    [Route("api/[controller]")]
+    public class PrivateStuffController : Controller
+    {
+        IConfiguration _configuration;
+
+        public PrivateStuffController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<string> Get(string value)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_configuration["PrivateAddress"]);
+                var response = await client.GetAsync(value);
+
+                return  await response.Content.ReadAsStringAsync();  
+            }
+        }
+    }
+}
